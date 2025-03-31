1c62451d4cfe4f2acda5fc8a2cee7d16ffc5956f
PankajJoshi-05 <joshipankaj9456@gmail.com>
2025-03-31 22:03:16 +0530

    initialized frontend

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { signup,login,logout,verifyEmail,forgotPassword,resetPassword} from "../controller/auth.controller.js";
-
+import { signup,login,logout,verifyEmail,forgotPassword,resetPassword,checkAuth} from "../controller/auth.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 const router=express.Router();
 
 router.post("/signup",signup);
@@ -9,4 +9,5 @@ router.post("/verify-email",verifyEmail);
 router.post("/logout",logout);
 router.post("/forgot-password",forgotPassword);
 router.post("/reset-password/:token",resetPassword);
+router.post("/check-auth",verifyToken,checkAuth);
 export default router;
