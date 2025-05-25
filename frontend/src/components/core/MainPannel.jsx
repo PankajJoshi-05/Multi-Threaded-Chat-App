4e6adca4206046fa04c6f0e4fa89b448577184d0
Harshit13-12 <harshitpandey1326@gmail.com>
2025-05-25 18:24:38 +0530

    MainPanel

--- frontend/src/components/core/MainPannel.jsx
+++ frontend/src/components/core/MainPannel.jsx
@@ -1,8 +1,7 @@
-import React,{ useState} from 'react'
 import {Outlet} from "react-router-dom"
 const MainPannel = () => {
   return (
-      <div className="h-full w-full md:w-[40%] lg:w-[30%] flex flex-col bg-base-200 text-base-content p-2 overflow-y-auto scrollbar-hide">
+      <div className="h-full w-full md:w-[40%] flex flex-col bg-base-200 text-base-content p-2 overflow-y-auto scrollbar-hide">
       <Outlet />
     </div>
   )
