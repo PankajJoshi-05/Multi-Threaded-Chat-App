c972c97772e5c7755d6024932efab8fbf11fd9d2
Jatin Mehra <dev.jatin.singh@gmail.com>
2025-05-25 17:50:39 +0530

    Add files via upload

--- backend/src/routes/chat.routes.js
+++ backend/src/routes/chat.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { verifyToken } from "../middleware/verifyToken.js";
-import {getChats,createGroup,changeGroupName,changeGroupProfile,addMembers,removeMember,getGroupMembers,leaveGroup,deleteGroup,getAllUsers,sendAttachments, sendVoiceMessage,getMessages} from "../controller/chat.controller.js";
+import {getChats,createGroup,changeGroupName,changeGroupProfile,addMembers,removeMember,getGroupMembers,leaveGroup,getAllUsers,sendAttachments, sendVoiceMessage,getMessages,deleteChat, changeGroupBio} from "../controller/chat.controller.js";
 import {singleUpload,multipleUpload} from "../middleware/multer.js";
 
 const router=express.Router();
@@ -8,12 +8,13 @@ const router=express.Router();
 router.get("/get-chats",verifyToken,getChats);
 router.post("/create-group",verifyToken,createGroup);
 router.put("/change-group-name",verifyToken,changeGroupName);
-router.put("/change-group-profile/:id",verifyToken,singleUpload,changeGroupProfile);
+router.put("/change-group-bio",verifyToken,changeGroupBio);
+router.put("/change-group-profile",verifyToken,singleUpload,changeGroupProfile);
 router.put("/add-members",verifyToken,addMembers);
 router.put("/remove-member",verifyToken,removeMember);
 router.get("/get-group-members",verifyToken,getGroupMembers);
 router.delete("/leave-group/:id",verifyToken,leaveGroup);
-router.delete("/delete-group/:id",verifyToken,deleteGroup);
+router.delete("/delete-chat/:id",verifyToken,deleteChat);
 router.get("/get-all-users",verifyToken,getAllUsers);
 
 router.put("/send-attachments",verifyToken,multipleUpload,sendAttachments);
