6e2ba54da89b7e329495f3542798a08565736ee2
Jatin Mehra <dev.jatin.singh@gmail.com>
2025-05-27 23:31:24 +0530

    NameChnage

--- frontend/src/components/core/Header.jsx
+++ frontend/src/components/core/Header.jsx
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
-import NotificationDropdown from '../ui/NotificationDropdown';
+import React, { useState } from "react";
+import NotificationDropdown from "../ui/NotificationDropdown";
 const Header = () => {
   return (
     <div className="w-full h-16 bg-base-200 text-base-content flex items-center justify-between px-6 shadow sticky top-0 z-50">
       <div className="flex items-center space-x-2">
-        <h1 className="text-2xl font-semibold">ChatApp</h1>
+        <h1 className="text-2xl font-semibold">ChatUp</h1>
       </div>
 
       <div className="flex items-center space-x-4">
-
         {/* Notifications */}
-          <NotificationDropdown />
+        <NotificationDropdown />
       </div>
     </div>
   );
